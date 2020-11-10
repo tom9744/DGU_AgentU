90bd085b7170ecd8dc334d821a5dfc17c5658fa3
Rush-K <k_rush@naver.com>
2020-11-10 17:11:24 +0900

    Cart Item delete
    Cart Item delete

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -61,4 +61,46 @@ Page({
 
   },
 
+  delFromCart (event) {
+    let delItem;
+    for (let index of this.data.items) { // 삭제할 항목을 탐색
+      if (index.menuId == event.target.id) {
+        delItem = index;
+      }
+    }
+    console.log(delItem);
+
+    wx.getStorage({
+      key: 'Cart',
+      success: ({ data }) => {
+        let currentCart = data;       // 현재 카트 상태를 저장한다.
+        for (let index = 0; index < currentCart.length; index++) { // 해당 항목을 찾아 삭제
+          if (currentCart[index].menuId == delItem.menuId) {
+            currentCart.splice(index, 1);
+            break;
+          }
+        }
+
+        let totalPrice = 0; 
+        // 카트에 담긴 항목들의 금액 합을 구한다.
+        for(let item of currentCart) {
+          totalPrice += parseInt(item.price);
+        }
+
+        wx.setStorage({ // 다시 저장
+          data: currentCart,
+          key: 'Cart',
+        });
+
+        this.setData({
+          totalPrice: totalPrice
+        });
+
+        console.log(currentCart);
+      }
+    });
+    wx.reLaunch({ // 페이지 새로고침
+      url: '/pages/cart/cart',
+    })
+  },
 })
