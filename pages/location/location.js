92d48c01f1d30b1363533d2487edbe40041cc0a4
Rush-K <k_rush@naver.com>
2020-11-20 20:20:48 +0900

    Pull
    Pull

--- pages/location/location.js
+++ pages/location/location.js
@@ -1,48 +1,132 @@
 // pages/location/location.js
+const checkOperation = (operatingTime) => {
+  // When a facility isn't running because of COVID-19.
+  if (operatingTime === null) {
+    return false;
+  }
+  else {
+    const isWeekdayOnly = operatingTime.split(" ")[0] === "(평일)";
+
+    let startTime = "";
+    let endTime = "";
+    let start = new Date();
+    let end = new Date();
+    
+    const now = new Date();
+    const day = now.getDay();
+
+    // [Pre-Processing] Remove "(평일)" from the string.
+    if (isWeekdayOnly) {
+      operatingTime = operatingTime.split(" ")[1];
+    }
+    startTime = operatingTime.split("~")[0];
+    endTime = operatingTime.split("~")[1];
+
+    start.setHours(startTime.split(":")[0]);
+    start.setMinutes(startTime.split(":")[1]);
+    end.setHours(endTime.split(":")[0]);
+    end.setMinutes(endTime.split(":")[1]);
+
+    // When the facility runs only on weekdays and today either Saturday or Sunday.
+    if (isWeekdayOnly && day === 0 || day === 6)
+    {
+      return false;
+    }
+    
+    // When the current time is inbetween opening & closing times.
+    if ( start <= now && now <= end ) {
+      return true;
+    }
+    
+    // When the current time hasn't reached the opening time or has passed the closing time.
+    return false;
+  }
+}
+
 Page({
   data: {
-    filterNumber: 0,
-    filter: ["전체", "식당", "보건소", "ATM", "은행", "편의점"],
+    facilityChinese: ["식당", "보건소", "ATM", "은행", "편의점", "우체국", "카페"],
+    facilityEnglish: ["food", "medic", "atm", "bank", "cvs", "post", "cafe"],
 
-    facility: [
-      {
-        facilityId : 0,
-        name: "상록원 학생식당",
-        location: "상록원",
-        type_name: "식당",
-        operatingTime: "09:00~17:00",
+    filterNumber: 0,
+    filter: ["전체", "식당", "보건소", "ATM", "은행", "편의점", "우체국", "카페"],
+
+    facility: [],
+  },
+  onLoad() {
+    console.log("The Page has been successfully loaded!");
+    
+    wx.request({
+      url: 'https://team1.miniform.kr:3000/api/facilities',
+      method: 'GET',
+      success: ({ data }) => {
+        console.log("Data from server has been successfully received!")
+        
+        // When the data object from server is empty.
+        if (data.length == 0) {
+          // Actiave a modal to print out the error message.
+          wx.showModal({
+            title: 'An Error has been occured!',
+            content: 'There is no data to fetch.',
+            showCancel: false,
+      
+            success (res) {
+              if (res.confirm) {
+                console.log('"OK" is tapped')
+              }
+            }
+          })
+        }
+        // When the data object from server has contents.
+        else {
+          // Make a new array to contain data.
+          let facilities = new Array();
+
+          // For each data content from the entire datasets,
+          for (const item of data) {
+            // Pasrse the Recieved Data into Proper JSON.
+            let facility = new Object();
+            const typeIndex = item.facility_type;
+  
+            // Add Props to a JSON Object.
+            facility.id = item.facility_id;
+            facility.name = item.facility_name;
+            facility.location = item.facility_location;
+            facility.category = this.data.facilityChinese[typeIndex - 1];
+            facility.operatingTime = item.facility_operation != null ? item.facility_operation : "운영중단";
+            facility.iconPath = `/resources/images/markers/${this.data.facilityEnglish[typeIndex - 1]}.svg`;
+            // facility.isOperating = checkOperation(item.facility_operation);
+            facility.isOperating = true;
+  
+            // Add a new array element to the JSON Object Array.
+            facilities.push(facility);
+          }
+          console.log(facilities);
+
+          this.setData({
+            facility: facilities
+          })
+        }
       },
-      {
-        facilityId : 1,
-        name: "기숙사 학생식당",
-        location: "신공학관",
-        type_name: "식당",
-        operatingTime: "09:00~17:00",
-      },
-      {
-        facilityId : 2,
-        name: "IBK ATM",
-        location: "신공학관",
-        type_name: "ATM",
-        operatingTime: "09:00~17:00",
-      },
-      {
-        facilityId : 3,
-        name: "CU 신공학관점",
-        location: "신공학관",
-        type_name: "편의점",
-        operatingTime: "09:00~17:00",
+      fail: ({ errMsg }) => {
+        // Parse the Error Message
+        errMsg = errMsg.split(":")[1].split("");
+        errMsg[0] = errMsg[0].toUpperCase();
+        errMsg = errMsg.join("");
+
+        wx.showModal({
+          title: 'An Error has been occured!',
+          content: errMsg,
+          showCancel: false,
+    
+          success (res) {
+            if (res.confirm) {
+              console.log('"OK" is tapped')
+            }
+          }
+        })
       }
-    ],
-  },
-  markertap(event) {
-    console.log(event);
-    this.setData({
-      selectedMarker: event.detail.markerId
-    });
-  },
-  regionChange(event) {
-    console.log(event);
+    })
   },
   onClick(event) {
     console.log(event.target.id);
@@ -54,13 +138,10 @@ Page({
   },
   gotoMenu(event) {
     const facilityId = event.target.id; 
-    const facilityData = this.data.facility[facilityId];
-
-    console.log(facilityData.title);
+    const facilityData = this.data.facility[facilityId - 1];
 
     wx.navigateTo({
-      // url: './detail/detail?item='+item,
-      url: '../../pages/menu/menu?title=' + facilityData.name + '&location=' + facilityData.location + '&operatingTime=' + facilityData.operatingTime
+      url: '../../pages/menu/menu?id=' + facilityId + '&title=' + facilityData.name + '&location=' + facilityData.location + '&operatingTime=' + facilityData.operatingTime
     })
-  }
+  },
 })
